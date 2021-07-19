bbe05a209a12062115ca4c972ceace3f5d973166
kobulwidodo <fadhilhan01@gmail.com>
2021-07-20 00:50:28 +0700

    Add `GET Book by ID`

--- src/handler.js
+++ src/handler.js
@@ -71,4 +71,26 @@ const getAllBookHandler = () => ({
   },
 });
 
-module.exports = { addNewBookHandler, getAllBookHandler };
+const getBookByIdHandler = (request, h) => {
+  const { bookId } = request.params;
+  const dataBook = book.filter((n) => n.id === bookId)[0];
+  if (dataBook !== undefined) {
+    const response = h.response({
+      status: 'success',
+      data: {
+        book: dataBook,
+      },
+    });
+    response.code(200);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
+module.exports = { addNewBookHandler, getAllBookHandler, getBookByIdHandler };
